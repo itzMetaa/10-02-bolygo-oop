77c1225110891f64e21cdd7cfb3626bdf8340c14
Diak <Diak@10.4.117.8>
2019-10-02 14:07:39 +0200

    initialize

---
+++ src/bolygooop/Bolygo.java
@@ -0,0 +1,5 @@
+package bolygooop;
+
+public class Bolygo {
+    
+}
---
+++ src/bolygooop/BolygoOOP.java
@@ -0,0 +1,9 @@
+package bolygooop;
+
+public class BolygoOOP {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
